532b31ce9b8442ef8349debf03e02f5b61f3a6a5
agandhi16 <alex_gandhi@asl.org>
2014-12-10 11:39:33 +0000

    Finished Code

--- Lab10.java
+++ Lab10.java
@@ -6,18 +6,43 @@
  *
  */
 
-import edu.fcps.karel2.Display; 
+import edu.fcps.karel2.Display;
+import javax.swing.JOptionPane;
+import edu.fcps.karel2.Robot;
 
 public class Lab10 {
 	 
 	 public static void main(String[] args) {
-		 //TODO Open the map 'maze1' or 'maze2' or 'maze3';
-		 //TODO Set the display to size 10 by 10
-		 Athlete athena = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
-		 clearMaze(athena);
+		String map = JOptionPane.showInputDialog("Which map?");
+		 Display.openWorld("maps/"+map+".map");
+		 Display.setSize(10, 10);
+       Display.setSpeed(8);
+		 Athlete alex = new Athlete(1, 1, Display.NORTH, Display.INFINITY);
+		 clearMaze(alex);
 	 }
 	 
 	 public static void clearMaze(Athlete arg) {
-		 //TODO Write an algorithm that directs arg to the beeper at the end of the maze
-	 }
- }
+	 while (!arg.nextToABeeper()){
+       
+       if (arg.frontIsClear() && !arg.leftIsClear()){
+       arg.move();
+       }
+       
+       if (arg.leftIsClear()){
+       arg.turnLeft();
+       arg.move();
+       }
+       
+       if (!arg.frontIsClear()&&!arg.leftIsClear()&&arg.rightIsClear()){
+       arg.turnRight();
+       arg.move();
+       }
+       
+       if (!arg.frontIsClear()&&!arg.leftIsClear()&&!arg.rightIsClear()){
+       arg.turnAround();
+       arg.move();
+       }
+
+}
+}
+}
